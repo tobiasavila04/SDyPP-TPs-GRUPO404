7e180f964ffa9b573d97312f4875de858dd41917
RenzRob <renzomartinrobles99@gmail.com>
2026-03-13 16:56:56 -0300

    test: break test to verify branch protection

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,7 +20,7 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "SD2026-GRUPO404 API is running"}
+    assert response.json() == {"message": "ESTE MENSAJE VA A FALLAR"}
 
 
 def test_health():
